7cab271b9fee94a1b4ca75c5e4478215489ce596
Jhuan Reategui Solis <chijure100x100to@gmail.com>
2020-06-27 11:57:54 -0500

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/app_citas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_citas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/default_theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+final defaulTheme = ThemeData.light().copyWith(
+  accentColor: Color.fromARGB(0, 61, 197, 255)
+);
